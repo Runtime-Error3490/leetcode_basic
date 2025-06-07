f687cdddf85c62eca675f68c827a9086fe3d8c72
Runtime-Error3490 <shaleen.shukla@learner.manipal.edu>
2025-06-07 22:50:23 +0530

    Update LeetCode user URL and enhance data extraction for global rank and attended contests

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ import puppeteer from "puppeteer";
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
-  await page.goto("https://leetcode.com/u/shaleen_shukla/", {
+  await page.goto("https://leetcode.com/u/shrudex/", {
     waitUntil: "networkidle2",
   });
   const ratingLabelSelector =
     "div.text-label-3.dark\\:text-dark-label-3.font-medium";
-  const globalRatingSelector = "div.text-label-3.dark\\:text-dark-label-3";
   const easyLabelSelector = "div.text-xs.font-medium.text-sd-easy";
   const mediumLabelSelector = "div.text-xs.font-medium.text-sd-medium";
   const hardLabelSelector = "div.text-xs.font-medium.text-sd-hard";
@@ -17,7 +16,6 @@ import puppeteer from "puppeteer";
   await page.waitForSelector(mediumLabelSelector, { timeout: 60000 });
   await page.waitForSelector(hardLabelSelector, { timeout: 60000 });
   await page.waitForSelector(ratingLabelSelector, { timeout: 60000 });
-  await page.waitForSelector(globalRatingSelector, { timeout: 60000 });
   const easyStat = await page.$eval(easyLabelSelector, (label) => {
     const countDiv = label.nextElementSibling;
     return {
@@ -46,13 +44,41 @@ import puppeteer from "puppeteer";
       count: countDiv ? countDiv.innerText.trim() : null,
     };
   });
-  const globalRating = await page.$eval(globalRatingSelector, (label) => {
-    const countDiv = label.nextElementSibling;
-    return {
-      label: label.innerText.trim(),
-      count: countDiv ? countDiv.innerText.trim() : null,
-    };
+  const globalRank = await page.evaluate(() => {
+    const label = Array.from(document.querySelectorAll("div")).find(
+      (el) => el.textContent.trim() === "Global Ranking"
+    );
+
+    return label && label.nextElementSibling
+      ? label.nextElementSibling.textContent.trim()
+      : null;
   });
+  const attendedContest = await page.evaluate(() => {
+    const label = Array.from(document.querySelectorAll("div")).find(
+      (el) => el.textContent.trim() === "Attended"
+    );
+    return label && label.nextElementSibling
+      ? label.nextElementSibling.textContent.trim()
+      : null;
+  });
+
+  const rank = await page.evaluate(() => {
+    const label = Array.from(document.querySelectorAll("span")).find(
+      (el) => el.textContent.trim() == "Rank"
+    );
+    return label && label.nextElementSibling
+      ? label.nextElementSibling.textContent.trim()
+      : null;
+  });
+
+  const avatarURL = await page.$eval(
+    "img.h-20.w-20.rounded-lg.object-cover",
+    (img) => img.src
+  );
+  console.log({ avatarURL });
+  console.log({ rank });
+  console.log({ attendedContest });
+  console.log({ globalRank });
   console.log(easyStat);
   console.log(mediumStat);
   console.log(hardStat);
